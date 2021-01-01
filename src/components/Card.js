f929359622d76d12d99d1c88b2e35f56f1764f22
Sushant <sushant.adhikari.619@gmail.com>
2021-01-01 09:47:40 +0545

    added user card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,26 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const { avatar_url, html_url, name, company, blog, bio, location, twitter_username } = githubUser;
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'John Doe'}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className="bio">{bio || 'I am a Software Engineer.'}</p>
+      <div className="links">
+        <p><MdBusiness></MdBusiness> {company || 'Student'}</p>
+        <p><MdLocationOn></MdLocationOn>{location || 'earth'}</p>
+        <a href={`https://${blog}`}><MdLink></MdLink>{blog || 'www.medium.com'}</a>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
